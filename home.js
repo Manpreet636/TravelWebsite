2c44a56144e9e95bd4de5f67703adf3766252d69
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2021-05-19 03:27:03 -0400

    Create home.js

---
+++ home.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.toggle');
+const page = document.querySelector('.page');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  page.classList.toggle('active');
+})
